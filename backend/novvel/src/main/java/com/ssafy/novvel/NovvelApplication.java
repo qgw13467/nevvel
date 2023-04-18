5876da8f8931db7f8fa437264cd6c495ee38b411
박희종 <qgw13467@naver.com>
2023-04-18 10:57:05 +0900

    feat(BE): add project

---
+++ backend/novvel/src/main/java/com/ssafy/novvel/NovvelApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.novvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovvelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovvelApplication.class, args);
+    }
+
+}
