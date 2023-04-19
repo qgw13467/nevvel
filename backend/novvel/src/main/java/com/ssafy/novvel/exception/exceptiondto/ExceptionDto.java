f9158d87692a5f15d8ed590768d6c57cc8a341e3
박희종 <qgw13467@naver.com>
2023-04-19 10:51:14 +0900

    feat: add exceptionhandler

---
+++ backend/novvel/src/main/java/com/ssafy/novvel/exception/exceptiondto/ExceptionDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.novvel.exception.exceptiondto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDto {
+    private String msg;
+    private String errorMsg;
+}
