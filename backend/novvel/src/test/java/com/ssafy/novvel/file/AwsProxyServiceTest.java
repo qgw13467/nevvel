645967815b28580e3f59a76e5185c28b39a5cb4d
박희종 <qgw13467@naver.com>
2023-04-19 15:36:23 +0900

    feat: modify AwsProxyServiceTest uploadFileTest

--- backend/novvel/src/test/java/com/ssafy/novvel/file/AwsProxyServiceTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/file/AwsProxyServiceTest.java
@@ -1,17 +1,13 @@
 package com.ssafy.novvel.file;
 
 import com.ssafy.novvel.file.service.AwsProxyService;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.UUID;
 
 @SpringBootTest
 public class AwsProxyServiceTest {
@@ -19,13 +15,14 @@ public class AwsProxyServiceTest {
     private AwsProxyService awsProxyService;
 
     @Test
-    @Disabled
+//    @Disabled
     @DisplayName("file upload test")
     void uploadFileTest() throws IOException {
-
         File file = new File("src/test/resources/test.gif");
-        String fileNamePrefix = "files/" + LocalDate.now(ZoneId.of("Asia/Seoul")) + UUID.randomUUID() + "-";
-        awsProxyService.uploadFile(file, fileNamePrefix + file.getName());
+        String fileNamePrefix = "files/" + LocalDate.of(2023,4,19) + "UUID" + "-";
+        String saveFile = awsProxyService.uploadFile(file, fileNamePrefix + file.getName());
+        File result = new File(saveFile);
+        result.delete();
 
     }
 }
