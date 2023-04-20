1b9469edeb97a9ab4c3ff4fa3be96320b5be892d
박희종 <qgw13467@naver.com>
2023-04-20 17:28:33 +0900

    feat: add TagRepository findSetByTagNameIn

--- backend/novvel/src/main/java/com/ssafy/novvel/asset/repository/TagRepository.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/asset/repository/TagRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
     List<Tag>  findByTagNameIn(List<String> tagNames);
+    Set<Tag> findSetByTagNameIn(List<String> tagNames);
 }
