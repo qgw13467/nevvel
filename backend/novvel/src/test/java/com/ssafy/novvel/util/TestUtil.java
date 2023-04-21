715d63da1cf38df26e4ce7bdd9dca13077cf102a
김호균 <h0kyungim@gmail.com>
2023-04-21 09:33:30 +0900

    test: add TestUser for test

--- backend/novvel/src/test/java/com/ssafy/novvel/util/TestUtil.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/util/TestUtil.java
@@ -1,17 +1,39 @@
 package com.ssafy.novvel.util;
 
 import com.ssafy.novvel.member.entity.Member;
+import java.util.Optional;
 
 public class TestUtil {
 
-    public static Member getMember(){
+    public static Member getMember() {
         return Member.builder()
-                .id(1L)
-                .email("email@naver.com")
-                .nickname("nickname")
-                .point(0L)
-                .build();
+            .id(1L)
+            .email("email@naver.com")
+            .nickname("nickname")
+            .point(0L)
+            .build();
     }
 
+    public static Optional<Member> getGUESTMember() {
+        return Optional.of(Member.builder()
+            .id(1L)
+            .email("guest@naver.com")
+            .role("ROLE_GUEST")
+            .sub("Kakao_2755574744")
+            .build()
+        );
+    }
+
+    public static Optional<Member> getUSERMember() {
+        return Optional.of(Member.builder()
+            .id(1L)
+            .email("user@naver.com")
+            .role("ROLE_USER")
+            .sub("Kakao_2755574745")
+            .nickname("user")
+            .point(0L)
+            .build()
+        );
+    }
 
 }
