6e1917f30583c9cf0ed5fd7b1a4cd50992a551d7
박희종 <qgw13467@naver.com>
2023-04-24 10:15:53 +0900

    fix: modify repositorytest datasource

--- backend/novvel/src/test/java/com/ssafy/novvel/asset/repository/AssetTagRepositoryTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/asset/repository/AssetTagRepositoryTest.java
@@ -7,6 +7,7 @@ import com.ssafy.novvel.asset.entity.Tag;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AssetTagRepositoryTest {
 
     @Autowired
