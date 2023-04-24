9d123241837cb110104e4251345e15b9e16b770e
박희종 <qgw13467@naver.com>
2023-04-24 20:29:36 +0900

    feat: add AssetSearchDto

--- backend/novvel/src/main/java/com/ssafy/novvel/asset/dto/AssetSearchDto.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/asset/dto/AssetSearchDto.java
@@ -1,4 +1,32 @@
 package com.ssafy.novvel.asset.dto;
 
+import com.ssafy.novvel.asset.entity.Asset;
+import com.ssafy.novvel.asset.entity.AssetTag;
+import com.ssafy.novvel.asset.entity.AssetType;
+import com.ssafy.novvel.asset.entity.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class AssetSearchDto {
+    private Long id;
+    private AssetType type;
+    private String thumbnail;
+    private Long price;
+    private List<Tag> tags;
+
+    public AssetSearchDto(Asset asset, List<Tag> assetTags) {
+        this.id = asset.getId();
+        this.type = asset.getType();
+        this.thumbnail = (asset.getResource().getThumbnailUrl() == null) ? "" : asset.getResource().getThumbnailUrl();
+        this.price = asset.getPoint();
+        this.tags = (assetTags == null) ? null : assetTags;
+    }
+
 }
