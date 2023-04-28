728148ea70b7e18729c1ee981e5320e6c18d8589
박희종 <qgw13467@naver.com>
2023-04-28 15:59:48 +0900

    fix: add findJoinMemberByAssets, searchAssetByUploader, findByAssetIn test

--- backend/novvel/src/test/java/com/ssafy/novvel/asset/service/AssetServiceTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/asset/service/AssetServiceTest.java
@@ -9,6 +9,7 @@ import com.ssafy.novvel.asset.entity.Tag;
 import com.ssafy.novvel.asset.repository.AssetRepository;
 import com.ssafy.novvel.asset.repository.AssetTagRepository;
 import com.ssafy.novvel.asset.repository.TagRepository;
+import com.ssafy.novvel.member.repository.MemberRepository;
 import com.ssafy.novvel.memberasset.entity.DealType;
 import com.ssafy.novvel.memberasset.entity.MemberAsset;
 import com.ssafy.novvel.memberasset.repository.MemberAssetRepository;
@@ -23,10 +24,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.*;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,6 +34,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
@@ -54,6 +53,8 @@ public class AssetServiceTest {
     private ResourceService resourceService;
     @Mock
     private MemberAssetRepository memberAssetRepository;
+    @Mock
+    private MemberRepository memberRepository;
 
     @Test
     void addAssetTest() throws IOException {
@@ -114,7 +115,7 @@ public class AssetServiceTest {
     void searchAssetByTagTest() {
 
         List<Member> members = TestUtil.getUSERMembers(4);
-        List<Asset> assets = TestUtil.getAssetList(4, members);
+        List<Asset> assets = TestUtil.getAssetList(4, members, TestUtil.getResources(4));
 
         Tag tag1 = new Tag(1L, "tag1");
         Tag tag2 = new Tag(2L, "tag2");
@@ -162,6 +163,55 @@ public class AssetServiceTest {
         Assertions.assertThat(assetSearchDtos.getContent().get(0).getIsAvailable()).isEqualTo(true);
         Assertions.assertThat(assetSearchDtos.getContent().get(1).getIsAvailable()).isEqualTo(true);
         Assertions.assertThat(assetSearchDtos.getContent().get(2).getIsAvailable()).isEqualTo(false);
+
+
+    }
+
+    @Test
+    void searchAssetByUploaderTest() {
+        List<Member> members = TestUtil.getUSERMembers(2);
+        List<Asset> assets = TestUtil.getAssetList(4, members.get(0), TestUtil.getResources(4));
+        Tag tag1 = new Tag(1L, "tag1");
+        Tag tag2 = new Tag(2L, "tag2");
+        Tag tag3 = new Tag(3L, "tag3");
+
+        List<AssetTag> findByAssetInResult = List.of(
+                new AssetTag(assets.get(0), tag1),
+                new AssetTag(assets.get(0), tag2),
+                new AssetTag(assets.get(1), tag2),
+                new AssetTag(assets.get(2), tag2),
+                new AssetTag(assets.get(3), tag3)
+        );
+        List<MemberAsset> findByMemberAndAssetInResult = TestUtil.getMemberAssets(members.get(1), assets);
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Asset> findByMemberResult = new PageImpl<>(
+                assets,
+                pageable,
+                pageable.getPageSize()
+        );
+
+        //when
+        Mockito.doReturn(Optional.of(members.get(0))).when(memberRepository).findById(Mockito.any(Long.class));
+        Mockito.doReturn(findByMemberResult).when(assetRepository).findByMember(members.get(0), pageable);
+        Mockito.doReturn(findByAssetInResult).when(assetTagRepository).findByAssetIn(findByMemberResult.getContent());
+        Mockito.doReturn(findByMemberAndAssetInResult).when(memberAssetRepository).findByMemberAndAssetIn(members.get(1), findByMemberResult.getContent());
+        Page<AssetSearchDto> assetSearchDtos = assetService.searchAssetByUploader(0L,members.get(1),pageable);
+
+        //then
+        Mockito.verify(memberRepository, Mockito.times(1)).findById(Mockito.any(Long.class));
+        Mockito.verify(assetRepository, Mockito.times(1)).findByMember(members.get(0), pageable);
+        Mockito.verify(assetTagRepository, Mockito.times(1)).findByAssetIn(findByMemberResult.getContent());
+        Mockito.verify(memberAssetRepository, Mockito.times(1)).findByMemberAndAssetIn(members.get(1), findByMemberResult.getContent());
+        Assertions.assertThat(assetSearchDtos.getContent().size()).isEqualTo(4);
+        Assertions.assertThat(assetSearchDtos.getContent().get(0).getIsAvailable()).isEqualTo(true);
+        Assertions.assertThat(assetSearchDtos.getContent().get(0).getTitle()).isEqualTo("0title");
+        Assertions.assertThat(assetSearchDtos.getContent().get(0).getUploader().getId()).isEqualTo(0L);
+        Assertions.assertThat(assetSearchDtos.getContent().get(0).getUploader().getNickname()).isEqualTo("0nickname");
+        Assertions.assertThat(assetSearchDtos.getContent().get(0).getUploader().getId()).isEqualTo(0L);
+        Assertions.assertThat(assetSearchDtos.getContent().get(1).getIsAvailable()).isEqualTo(false);
+        Assertions.assertThat(assetSearchDtos.getContent().get(2).getIsAvailable()).isEqualTo(true);
+        Assertions.assertThat(assetSearchDtos.getContent().get(3).getIsAvailable()).isEqualTo(false);
 
 
     }
