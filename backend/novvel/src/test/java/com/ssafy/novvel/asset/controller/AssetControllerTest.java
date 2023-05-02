7f46a7df65af8054710896111c2a8cbc18b0c0e0
박희종 <qgw13467@naver.com>
2023-05-02 10:42:44 +0900

    feat: add WithMockCustomUser anotation

--- backend/novvel/src/test/java/com/ssafy/novvel/asset/controller/AssetControllerTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/asset/controller/AssetControllerTest.java
@@ -6,7 +6,7 @@ import com.ssafy.novvel.asset.entity.AssetType;
 import com.ssafy.novvel.member.entity.Member;
 import com.ssafy.novvel.member.repository.MemberRepository;
 import com.ssafy.novvel.util.TestUtil;
-import com.ssafy.novvel.util.token.CustomUserDetails;
+import com.ssafy.novvel.util.WithMockCustomUser;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,8 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -38,19 +36,20 @@ public class AssetControllerTest {
     private MockMvc mockMvc;
     Member member = TestUtil.getTestMember();
 
-    @BeforeEach
-    void setContext(){
-        CustomUserDetails customUserDetails = new CustomUserDetails(member);
-        SecurityContextHolder.getContext()
-                .setAuthentication(new UsernamePasswordAuthenticationToken(customUserDetails, null,
-                        customUserDetails.getAuthorities()));
-    }
+//    @BeforeEach
+//    void setContext(){
+//        CustomUserDetails customUserDetails = new CustomUserDetails(member);
+//        SecurityContextHolder.getContext()
+//                .setAuthentication(new UsernamePasswordAuthenticationToken(customUserDetails, null,
+//                        customUserDetails.getAuthorities()));
+//    }
 
     @Test
     @Transactional
     @DisplayName("registAsset Test")
 //    @WithMockUser(roles = "USER")
 //    @WithUserDetails("Kakao_2755574745")
+    @WithMockCustomUser
     void registAssetTest() throws Exception {
 
         //given
