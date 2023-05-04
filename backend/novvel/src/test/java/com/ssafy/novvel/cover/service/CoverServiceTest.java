5cd9cf754ffd3b9e3d84879e2e4ec372336d6223
김호균 <h0kyungim@gmail.com>
2023-05-04 10:21:17 +0900

    test: Added test code for the getAllEpisode method in CoverService

--- backend/novvel/src/test/java/com/ssafy/novvel/cover/service/CoverServiceTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/cover/service/CoverServiceTest.java
@@ -1,17 +1,25 @@
 package com.ssafy.novvel.cover.service;
 
+import com.ssafy.novvel.cover.dto.CoverInfoAndEpisodesDto;
 import com.ssafy.novvel.cover.dto.CoverRegisterDto;
+import com.ssafy.novvel.cover.dto.EpisodeInfoDto;
 import com.ssafy.novvel.cover.entity.Cover;
 import com.ssafy.novvel.cover.entity.CoverStatusType;
 import com.ssafy.novvel.cover.repository.CoverRepository;
 import com.ssafy.novvel.genre.entity.Genre;
 import com.ssafy.novvel.genre.repository.GenreRepository;
+import com.ssafy.novvel.member.entity.Member;
 import com.ssafy.novvel.resource.entity.Resource;
 import com.ssafy.novvel.resource.service.ResourceService;
+import com.ssafy.novvel.transactionhistory.entity.PointChangeType;
 import com.ssafy.novvel.util.TestUtil;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -53,7 +61,7 @@ class CoverServiceTest {
 
         Cover expect = Cover.builder()
             .id(1L).coverStatusType(CoverStatusType.SERIALIZED).genre(genre)
-            .member(TestUtil.getUSERMember().get()).resource(resource)
+            .member(TestUtil.getUSERMember().orElse(null)).resource(resource)
             .title(coverRegisterDto.getTitle()).build();
 
         // when
@@ -62,7 +70,7 @@ class CoverServiceTest {
             .getReferenceById(Mockito.any());
         Mockito.doReturn(expect).when(coverRepository).save(Mockito.any());
         Cover result = coverService.registerCover(multipartFile, coverRegisterDto,
-            TestUtil.getUSERMember().get());
+            TestUtil.getUSERMember().orElse(null));
 
         // then
         Assertions.assertThat(result.getCoverStatusType()).isEqualTo(expect.getCoverStatusType());
@@ -72,4 +80,56 @@ class CoverServiceTest {
 
     }
 
+    @Test
+    void getAllEpisode() {
+
+        // given
+        Member member = TestUtil.getMember();
+        Genre genre = Genre.builder().id(1L).name("test_genre").build();
+        Optional<Cover> given = Optional.of(Cover.builder()
+            .id(1L).coverStatusType(CoverStatusType.SERIALIZED).genre(genre)
+            .member(TestUtil.getUSERMember().orElse(null))
+            .title("test").build());
+
+        List<EpisodeInfoDto> list = new ArrayList<>();
+        list.add(new EpisodeInfoDto(1L, 0L, 0L, LocalDateTime.now(),
+            PointChangeType.BUY_EPISODE, true));
+        list.add(new EpisodeInfoDto(2L, 0L, 0L, LocalDateTime.now(),
+            PointChangeType.SELL_EPISODE, true));
+
+        CoverInfoAndEpisodesDto expect = CoverInfoAndEpisodesDto.builder()
+            .episodes(list)
+            .genreName(given.get().getGenre().getName())
+            .title(given.get().getTitle())
+            .build();
+
+        // when
+        Mockito.doReturn(given).when(coverRepository).findById(1L);
+        Mockito.doReturn(list).when(coverRepository).findEpisodesInfoDto(1L, member.getId());
+        CoverInfoAndEpisodesDto result = coverService.getAllEpisodes(1L, member.getId());
+
+        // then
+        Assertions.assertThat(result.getTitle()).isEqualTo(expect.getTitle());
+        Assertions.assertThat(result.getDescription()).isEqualTo(expect.getDescription());
+        Assertions.assertThat(result.getGenreName()).isEqualTo(expect.getGenreName());
+        Assertions.assertThat(result.getEpisodes().size()).isEqualTo(expect.getEpisodes().size());
+        Assertions.assertThat(result.getEpisodes().get(0).getIsPurchased()).isEqualTo(true);
+        Assertions.assertThat(result.getEpisodes().get(1).getIsPurchased()).isEqualTo(false);
+
+    }
+
+    @Test
+    void getAllEpisodeNotExistCover() {
+
+        // given
+        Member member = TestUtil.getMember();
+
+        Optional<Cover> given = Optional.empty();
+
+        // when
+        Mockito.doReturn(given).when(coverRepository).findById(1L);
+        Assertions.assertThatThrownBy(() -> coverService.getAllEpisodes(1L, member.getId()))
+            .isInstanceOf(NullPointerException.class);
+
+    }
 }
