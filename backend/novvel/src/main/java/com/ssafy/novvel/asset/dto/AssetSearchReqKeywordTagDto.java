018bdd58bd31d468aeb7f229420b6788baf181a7
박희종 <qgw13467@naver.com>
2023-05-08 17:15:54 +0900

    feat: add AssetSearchReqDto setter

--- backend/novvel/src/main/java/com/ssafy/novvel/asset/dto/AssetSearchReqKeywordTagDto.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/asset/dto/AssetSearchReqKeywordTagDto.java
@@ -4,12 +4,14 @@ import com.ssafy.novvel.asset.entity.AssetType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class AssetSearchReqKeywordTagDto {
     private String keyword;
     private List<String> tags;
