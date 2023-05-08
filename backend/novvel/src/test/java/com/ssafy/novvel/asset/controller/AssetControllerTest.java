e5568425e14f8d9d6869592403ea039bb7c0e59b
박희종 <qgw13467@naver.com>
2023-05-09 00:29:10 +0900

    feat: add updateAssetTest

--- backend/novvel/src/test/java/com/ssafy/novvel/asset/controller/AssetControllerTest.java
+++ backend/novvel/src/test/java/com/ssafy/novvel/asset/controller/AssetControllerTest.java
@@ -2,11 +2,22 @@ package com.ssafy.novvel.asset.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.novvel.asset.dto.AssetRegistDto;
+import com.ssafy.novvel.asset.entity.Asset;
+import com.ssafy.novvel.asset.entity.AssetTag;
 import com.ssafy.novvel.asset.entity.AssetType;
+import com.ssafy.novvel.asset.entity.Tag;
+import com.ssafy.novvel.asset.repository.AssetRepository;
+import com.ssafy.novvel.asset.repository.AssetTagRepository;
+import com.ssafy.novvel.asset.repository.TagRepository;
 import com.ssafy.novvel.member.entity.Member;
 import com.ssafy.novvel.member.repository.MemberRepository;
+import com.ssafy.novvel.memberasset.repository.MemberAssetRepository;
+import com.ssafy.novvel.resource.entity.Resource;
+import com.ssafy.novvel.resource.repository.ResourceRepository;
 import com.ssafy.novvel.util.TestUtil;
 import com.ssafy.novvel.util.WithMockCustomUser;
+import com.ssafy.novvel.util.token.CustomUserDetails;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,10 +25,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -26,10 +38,21 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("integration")
 public class AssetControllerTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private AssetRepository assetRepository;
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private MemberAssetRepository memberAssetRepository;
+    @Autowired
+    private AssetTagRepository assetTagRepository;
+    @Autowired
+    private ResourceRepository resourceRepository;
     @Autowired
     private ObjectMapper mapper;
     @Autowired
@@ -45,7 +68,6 @@ public class AssetControllerTest {
 //    }
 
     @Test
-    @Transactional
     @DisplayName("registAsset Test")
 //    @WithMockUser(roles = "USER")
 //    @WithUserDetails("Kakao_2755574745")
@@ -87,7 +109,6 @@ public class AssetControllerTest {
     }
 
     @Test
-    @Transactional
     @DisplayName("searchByMemberId Test")
 //    @WithMockUser(roles = "USER")
 //    @WithUserDetails("Kakao_2755574745")
@@ -127,4 +148,51 @@ public class AssetControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk());
 //                .andExpect(MockMvcResultMatchers.content().string(hello)); //응답 본문의 내용을 검증
     }
+
+    @Test
+    @DisplayName("updateAssetTest")
+//    @WithMockUser(roles = "USER")
+//    @WithUserDetails("Kakao_2755574745")
+    @WithMockCustomUser
+    void updateAssetTest() throws Exception {
+
+        //given
+        member = memberRepository.save(member);
+        Resource resource = TestUtil.getResource();
+        resource = resourceRepository.save(resource);
+        Asset asset = Asset.builder()
+                .title("title")
+                .description("desc")
+                .point(10L)
+                .type(AssetType.IMAGE)
+                .member(member)
+                .resource(resource)
+                .downloadCount(0L)
+                .build();
+        assetRepository.save(asset);
+        List<Tag> tags = TestUtil.getTagIntegList(2);
+        tags = tagRepository.saveAll(tags);
+        List<AssetTag> assetTags = List.of(
+                new AssetTag(asset, tags.get(0)),
+                new AssetTag(asset, tags.get(1))
+        );
+        assetTags = assetTagRepository.saveAll(assetTags);
+        AssetRegistDto requestDto =
+                new AssetRegistDto(AssetType.IMAGE, "newTitle", "newDescription", 100L, List.of("깝놀", "귀신"));
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.put("/assets/"+asset.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(requestDto))
+                        .with(csrf())
+                )
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+
+
+
+
+
 }
