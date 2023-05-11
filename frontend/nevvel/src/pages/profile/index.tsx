186583e99fc7b378c150f850aa94a3b440d38512
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-05-11 17:01:21 +0900

    feat : 로그인 localStorage 설정 - atomWithStorage 이용

--- frontend/nevvel/src/pages/profile/index.tsx
+++ frontend/nevvel/src/pages/profile/index.tsx
@@ -1,9 +1,30 @@
+import { loginAtom } from "@/src/store/Login";
+import { useAtomValue } from "jotai";
 import styled from "styled-components";
 
 function Profile() {
-  return <Wrapper>프로필</Wrapper>;
+  const loginStatus = useAtomValue(loginAtom);
+
+  return (
+    <Wrapper>
+      {loginStatus ? (
+        <ProfileTitle>프로필</ProfileTitle>
+      ) : (
+        <ProfileTitle>로그인 하세요</ProfileTitle>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Profile;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-left: 30%;
+  margin-right: 30%;
+  margin-top: 5rem;
+`;
+
+const ProfileTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 800;
+`;
