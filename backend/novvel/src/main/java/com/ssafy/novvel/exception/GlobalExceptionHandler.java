200f815a0e5ff1670ec8ee2f2c7f2e817fbd7419
박희종 <qgw13467@naver.com>
2023-05-11 15:12:28 +0900

    fix: resolve conflict

--- backend/novvel/src/main/java/com/ssafy/novvel/exception/GlobalExceptionHandler.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.io.IOException;
 
