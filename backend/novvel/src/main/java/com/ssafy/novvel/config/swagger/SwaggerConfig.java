64660824662a540aa5afc91881c02965007cfdc4
Lee Sijun <season2lee@gmail.com>
2023-05-12 13:47:02 +0900

    fix: swagger fix

--- backend/novvel/src/main/java/com/ssafy/novvel/config/swagger/SwaggerConfig.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/config/swagger/SwaggerConfig.java
@@ -20,10 +20,6 @@ public class SwaggerConfig {
                 .description("SSAFY 자율 프로젝트 D106팀의 Nevvel API 입니다.");
 
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
-                .components(new Components().addSecuritySchemes("Authorization",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
-                                .bearerFormat("JWT")))//JWT 전역 설정을 위해 추가
                 .info(info);
     }
 }
