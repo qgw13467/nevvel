f3242343d7c7f6a6dc51fbff804cc81c83332ff4
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-05-14 01:44:03 +0900

    feat: 네브바 선택 시 색 변경 - 네브바 border 색상 동일하게 변경

--- frontend/nevvel/src/components/layout/NavigationBottom.tsx
+++ frontend/nevvel/src/components/layout/NavigationBottom.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
@@ -15,6 +16,9 @@ function NavigationBottom() {
   const router = useRouter();
   const value = useAtomValue(themeAtom);
 
+  // 소설 목록 클릭했을 때 배경색 주기
+  const [clicked, setClicked] = useState<string | null>(null);
+
   const genreSelectHandler = () => {
     router.push(
       {
@@ -23,6 +27,7 @@ function NavigationBottom() {
       }
       // `/novels/genres`
     );
+    setClicked("genre");
   };
 
   const completedSelectHandler = () => {
@@ -33,6 +38,7 @@ function NavigationBottom() {
       }
       // `/novels/completed`
     );
+    setClicked("completed");
   };
 
   const latestSelectHandler = () => {
@@ -43,35 +49,68 @@ function NavigationBottom() {
       }
       // `/novels/latest`
     );
+    setClicked("latest");
+  };
+
+  // assetstore 클릭 시 색상 초기화 설정을 위한 handler
+  const assetstoreHandler = () => {
+    setClicked(null);
+  };
+
+  // Logo 클릭 시 색상 초기화 설정을 위한 handler
+  const logoHandler = () => {
+    setClicked(null);
   };
 
   return (
-    <Wrapper>
-      <Link href="/">
-        {value === "light" ? (
-          <Image src={nevvel_light} alt="Logo" width={100} height={25} />
-        ) : (
-          <Image src={nevvel_dark} alt="Logo" width={100} height={25} />
-        )}
-      </Link>
+    <Wrapper clicked={clicked === null}>
+      <LogoNav onClick={logoHandler}>
+        <Link href="/">
+          {value === "light" ? (
+            <Image src={nevvel_light} alt="Logo" width={100} height={25} />
+          ) : (
+            <Image src={nevvel_dark} alt="Logo" width={100} height={25} />
+          )}
+        </Link>
+      </LogoNav>
       <Novel>
-        <Genre onClick={genreSelectHandler}>장르별 소설</Genre>
-        <Completed onClick={completedSelectHandler}>완결 소설</Completed>
-        <Latest onClick={latestSelectHandler}>최신 소설</Latest>
-        <AssetStore>
-          <Link href="/assetstore/assetstore">에셋 스토어</Link>
-        </AssetStore>
+        <BackgroundDiv>
+          <NavList clicked={clicked === "genre"} onClick={genreSelectHandler}>
+            장르별 소설
+          </NavList>
+        </BackgroundDiv>
+        <BackgroundDiv>
+          <NavList
+            clicked={clicked === "completed"}
+            onClick={completedSelectHandler}
+          >
+            완결 소설
+          </NavList>
+        </BackgroundDiv>
+        <BackgroundDiv>
+          <NavList clicked={clicked === "latest"} onClick={latestSelectHandler}>
+            최신 소설
+          </NavList>
+        </BackgroundDiv>
+        <BackgroundDiv>
+          <AssetStore onClick={assetstoreHandler}>
+            <Link href="/assetstore/assetstore">에셋 스토어</Link>
+          </AssetStore>
+        </BackgroundDiv>
       </Novel>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ clicked: boolean }>`
   display: flex;
   align-items: center;
   height: 3.75rem;
   padding-left: 2rem;
   padding-right: 8rem;
+
+  border-bottom: 0.5px solid
+    ${({ clicked, theme }) => (clicked ? "#e6e6e6" : theme.color.subNavbar)};
 
   ${tabletH} {
     padding-right: 12%;
@@ -79,6 +118,10 @@ const Wrapper = styled.div`
   }
 
   ${mobile}
+`;
+
+const LogoNav = styled.div`
+  margin-top: 0.75rem;
 `;
 
 const Novel = styled.div`
@@ -100,18 +143,29 @@ const Novel = styled.div`
   }
 `;
 
-const Genre = styled.div`
-  cursor: pointer;
+const BackgroundDiv = styled.div`
+  display: flex;
+  width: 25%;
+  height: 3rem;
+  line-height: 3rem;
+  margin-top: 0.75rem;
 `;
 
-const Completed = styled.div`
+const NavList = styled.div<{ clicked: boolean }>`
   cursor: pointer;
+  text-align: center;
+  justify-content: center;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  background-color: ${({ clicked, theme }) =>
+    clicked ? theme.color.subNavbar : ""};
+  color: ${({ clicked, theme }) =>
+    clicked ? theme.color.text2 : theme.color.text1};
 `;
 
-const Latest = styled.div`
-  cursor: pointer;
+const AssetStore = styled.div`
+  text-align: center;
+  width: 100%;
 `;
-
-const AssetStore = styled.div``;
 
 export default NavigationBottom;
