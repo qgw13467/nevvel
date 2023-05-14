9d883faacd4816b2c17c2e75bbf661f5c9c3091c
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-05-14 13:17:02 +0900

    feat : 로그인 페이지 구현

--- frontend/nevvel/src/pages/login/index.tsx
+++ frontend/nevvel/src/pages/login/index.tsx
@@ -1,11 +1,25 @@
+import { useRouter } from "next/dist/client/router";
+import Image from "next/image";
+import nevvel_m_dark from "../../assets/img/nevvel_m_dark.png";
+import kakao_login from "../../assets/img/kakao_login_medium_narrow.png";
 import styled from "styled-components";
 
 function Login() {
-  const kakaoLogin = "http://k8d1061.p.ssafy.io/api/oauth2/authorization/kakao";
+  const router = useRouter();
+
+  const kakaoLogin = () => {
+    router.push("http://k8d1061.p.ssafy.io/api/oauth2/authorization/kakao");
+  };
 
   return (
     <Wrapper>
-      <LoginBtn href={kakaoLogin}>카카오 로그인</LoginBtn>
+      <ImageWrapper>
+        <Image src={nevvel_m_dark} alt="로그인" width={300} height={300} />
+      </ImageWrapper>
+      <LoginText>Nevvel 로그인</LoginText>
+      <LoginImage>
+        <Image onClick={kakaoLogin} src={kakao_login} alt="카카오 로그인" />
+      </LoginImage>
     </Wrapper>
   );
 }
@@ -13,11 +27,25 @@ function Login() {
 export default Login;
 
 const Wrapper = styled.div`
-  padding: 0;
-  margin: 0;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.color.background};
-  color: ${({ theme }) => theme.color.text1};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-const LoginBtn = styled.a``;
+const ImageWrapper = styled.div`
+  margin-top: 5rem;
+  opacity: 0.3;
+`;
+
+const LoginText = styled.div`
+  position: absolute;
+  margin-top: -3rem;
+  font-size: 20px;
+  font-weight: 800;
+`;
+
+const LoginImage = styled.div`
+  position: absolute;
+  margin-top: 10rem;
+`;
