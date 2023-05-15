a98a44eab68ad48e25bcc4d9f464d815ee107bc3
YuhongKim <loyyir1223@naver.com>
2023-05-15 12:35:58 +0900

    axios test13

--- frontend/nevvel/src/components/assetstore/ImgUpload.tsx
+++ frontend/nevvel/src/components/assetstore/ImgUpload.tsx
@@ -132,9 +132,9 @@ function ImgUpload(props:assetstoreProps) {
       // 제출버튼 누르면 formdata에 데이터 집어넣기
       if (image) {
         formData.append('file', image)
-        // formData.append('assetRegistDto', JSON.stringify(jsonDatas))
       }
-      formData.append('assetRegistDto', new Blob([JSON.stringify(jsonDatas)], {type: "application/json"}))
+      formData.append('assetRegistDto', JSON.stringify(jsonDatas))
+      // formData.append('assetRegistDto', new Blob([JSON.stringify(jsonDatas)], {type: "application/json"}))
       
       // 데이터 집어넣어진 다음 모달 열기
       setModalonModalOpen(true)
@@ -149,6 +149,7 @@ function ImgUpload(props:assetstoreProps) {
 
   useEffect(()=>{
     console.log(jsonDatas)
+  
   },[jsonDatas])
 
   
