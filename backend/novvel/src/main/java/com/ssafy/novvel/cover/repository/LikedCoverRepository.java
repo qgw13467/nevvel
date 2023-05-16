292db06834f4810e8d3b28f2afcab80fbc13dee1
박희종 <qgw13467@naver.com>
2023-05-16 23:24:13 +0900

    feat: add cover favorite

---
+++ backend/novvel/src/main/java/com/ssafy/novvel/cover/repository/LikedCoverRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.novvel.cover.repository;
+
+
+import com.ssafy.novvel.cover.entity.Cover;
+import com.ssafy.novvel.cover.entity.CoverMemberID;
+import com.ssafy.novvel.cover.entity.LikedCover;
+import com.ssafy.novvel.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikedCoverRepository extends JpaRepository<LikedCover, CoverMemberID> {
+    Optional<LikedCover> findByMemberAndCoverId(Member member, Long coverId);
+
+}
