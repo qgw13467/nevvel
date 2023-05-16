d8cb3404aa3dcaba041f504f681441cd5f2440b3
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-05-17 01:24:34 +0900

    feat : 마이페이지 구매한 에셋 axios 연결 - 포인트 글자색, 배경색 수정 - 마이페이지 구매한 에셋 axios 연결 - 프로필 description 받아오기

--- frontend/nevvel/src/components/mypage/MyAsset.tsx
+++ frontend/nevvel/src/components/mypage/MyAsset.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import SemiTitle from "./SemiTitle";
 import styled from "styled-components";
+import { NewvelApi } from "@/src/api";
 
 function MyAsset() {
   // 구매한 에셋
+  const [purchasedAsset, setPurchasedAsset] = useState();
+  useEffect(() => {
+    const getPurchasedAssets = async () => {
+      const res = await NewvelApi.purchasedAssets();
+      console.log(res.data);
+    };
+    getPurchasedAssets();
+  }, []);
 
   // 만든 에셋
   return (
