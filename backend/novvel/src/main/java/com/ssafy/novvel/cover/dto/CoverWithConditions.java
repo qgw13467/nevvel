d775e72938096397c6f597fae5f4a812fe229a74
김호균 <h0kyungim@gmail.com>
2023-05-17 12:43:45 +0900

    feat: Added cover default image

--- backend/novvel/src/main/java/com/ssafy/novvel/cover/dto/CoverWithConditions.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/cover/dto/CoverWithConditions.java
@@ -3,6 +3,7 @@ package com.ssafy.novvel.cover.dto;
 import com.querydsl.core.annotations.QueryProjection;
 import com.ssafy.novvel.cover.entity.Cover;
 import com.ssafy.novvel.cover.entity.CoverStatusType;
+import com.ssafy.novvel.cover.util.DefaultImage;
 import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,14 +25,13 @@ public class CoverWithConditions {
     private Boolean isNew;
 
     @Builder
-    //@QueryProjection
     public CoverWithConditions(Long id, String title,
         CoverStatusType status, String thumbnail, String genre, Long writerId,
         String writerNickname, Boolean isUploaded, Boolean isNew) {
         this.id = id;
         this.title = title;
         this.status = status;
-        this.thumbnail = thumbnail;
+        this.thumbnail = checkThumbnail(thumbnail, genre);
         this.genre = genre;
         this.writer = new CoverWriter(writerId, writerNickname);
         this.isUploaded = isUploaded;
@@ -44,13 +44,23 @@ public class CoverWithConditions {
         this.id = cover.getId();
         this.title = cover.getTitle();
         this.status = cover.getCoverStatusType();
-        this.thumbnail = cover.getResource().getThumbnailUrl();
+        this.thumbnail = checkThumbnail(cover.getResource().getThumbnailUrl(),
+            cover.getGenre().getName());
         this.genre = cover.getGenre().getName();
         this.writer = new CoverWriter(cover.getMember().getId(), cover.getMember().getNickname());
-        if(cover.getLastPublishDate() == null) this.isUploaded = false;
-        else this.isUploaded = cover.getLastPublishDate().isAfter(LocalDate.now().minusDays(7L));
-        if(cover.getFirstPublishDate() == null) this.isNew = false;
-        else this.isNew = cover.getFirstPublishDate().isAfter(LocalDate.now().minusDays(7L));
+        if (cover.getLastPublishDate() == null) {
+            this.isUploaded = false;
+        } else {
+            this.isUploaded = cover.getLastPublishDate().isAfter(LocalDate.now().minusDays(7L));
+        }
+        if (cover.getFirstPublishDate() == null) {
+            this.isNew = false;
+        } else {
+            this.isNew = cover.getFirstPublishDate().isAfter(LocalDate.now().minusDays(7L));
+        }
     }
 
+    private String checkThumbnail(String thumbnail, String genreName) {
+        return thumbnail == null ? new DefaultImage().getImageByGenreName(genreName) : thumbnail;
+    }
 }
--- backend/novvel/src/main/java/com/ssafy/novvel/cover/service/CoverServiceImpl.java
+++ backend/novvel/src/main/java/com/ssafy/novvel/cover/service/CoverServiceImpl.java
@@ -11,6 +11,7 @@ import com.ssafy.novvel.cover.repository.LikedCoverRepository;
 import com.ssafy.novvel.cover.repository.CoverRepository;
 import com.ssafy.novvel.cover.dto.CoverRegisterDto;
 import com.ssafy.novvel.cover.entity.Cover;
+import com.ssafy.novvel.cover.util.DefaultImage;
 import com.ssafy.novvel.episode.entity.ReadEpisode;
 import com.ssafy.novvel.episode.repository.ReadEpisodeRepository;
 import com.ssafy.novvel.exception.NotFoundException;
@@ -19,10 +20,12 @@ import com.ssafy.novvel.genre.repository.GenreRepository;
 import com.ssafy.novvel.member.entity.Member;
 import com.ssafy.novvel.member.repository.MemberRepository;
 import com.ssafy.novvel.resource.entity.Resource;
+import com.ssafy.novvel.resource.repository.ResourceRepository;
 import com.ssafy.novvel.resource.service.ResourceService;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
@@ -40,20 +43,26 @@ public class CoverServiceImpl implements CoverService {
     private final ResourceService resourceService;
     private final GenreRepository genreRepository;
     private final CoverRepository coverRepository;
+    private final ResourceRepository resourceRepository;
     private final ReadEpisodeRepository readEpisodeRepository;
+    private final DefaultImage defaultImage;
     private final LikedCoverRepository likedCoverRepository;
 
     public CoverServiceImpl(
             ResourceService resourceService,
+            ResourceRepository resourceRepository,
             GenreRepository genreRepository,
             CoverRepository coverRepository,
             ReadEpisodeRepository readEpisodeRepository,
             LikedCoverRepository likedCoverRepository,
-            MemberRepository memberRepository) {
+            MemberRepository memberRepository,
+            DefaultImage defaultImage) {
         this.resourceService = resourceService;
         this.genreRepository = genreRepository;
         this.coverRepository = coverRepository;
+        this.resourceRepository = resourceRepository;
         this.readEpisodeRepository = readEpisodeRepository;
+        this.defaultImage = defaultImage;
         this.likedCoverRepository = likedCoverRepository;
         this.memberRepository = memberRepository;
     }
@@ -61,9 +70,11 @@ public class CoverServiceImpl implements CoverService {
     @Override
     @Transactional
     public Cover registerCover(MultipartFile multipartFile, CoverRegisterDto coverRegisterDto,
-                               Member member) throws IOException, EntityNotFoundException {
-
-        Resource resource = resourceService.saveFile(multipartFile);
+        Member member) throws IOException, EntityNotFoundException {
+        Resource resource = null;
+        if (multipartFile != null && !multipartFile.isEmpty()) {
+            resource = resourceService.saveFile(multipartFile);
+        }
 
         return coverRepository.save(new Cover(resource, coverRegisterDto, member,
                 genreRepository.getReferenceById(coverRegisterDto.getGenreId())));
@@ -78,14 +89,20 @@ public class CoverServiceImpl implements CoverService {
         coverInfoAndEpisodesDto.setTitle(cover.getTitle());
         coverInfoAndEpisodesDto.setDescription(cover.getDescription());
         coverInfoAndEpisodesDto.setGenre(cover.getGenre().getName());
-        coverInfoAndEpisodesDto.setThumbnail(cover.getResource().getThumbnailUrl());
+        String thumbnail;
+        if(cover.getResource() == null || cover.getResource().getThumbnailUrl() == null) {
+            thumbnail = defaultImage.getImageByGenreName(cover.getGenre().getName());
+        } else {
+            thumbnail = cover.getResource().getThumbnailUrl();
+        }
+        coverInfoAndEpisodesDto.setThumbnail(thumbnail);
         coverInfoAndEpisodesDto.setWriter(
-                new CoverWriter(cover.getMember().getId(), cover.getMember().getNickname()));
+            new CoverWriter(cover.getMember().getId(), cover.getMember().getNickname()));
 
         ReadEpisode readEpisode = null;
-        if(member != null) {
-            readEpisode = readEpisodeRepository.findFirstByMember_IdAndEpisode_Cover_IdOrderByLastModifyedDateTimeDesc(
-                member.getId(), coverId);
+        if (member != null) {
+            readEpisode = readEpisodeRepository.findTopByMemberIdOrderByLastModifyedDateTimeDesc(
+                member.getId()).orElse(null);
         }
         Long lastReadEpisodeId = null;
         if (readEpisode != null) {
@@ -100,8 +117,8 @@ public class CoverServiceImpl implements CoverService {
     @Override
     @Transactional
     public Resource updateCover(MultipartFile multipartFile, Long coverId,
-                                CoverModifyDto coverModifyDto,
-                                Long userId) throws NotYourAuthorizationException, IOException {
+        CoverModifyDto coverModifyDto,
+        Long userId) throws NotYourAuthorizationException, IOException {
 
         Cover cover = coverRepository.findById(coverId).orElseThrow(NullPointerException::new);
 
@@ -112,28 +129,28 @@ public class CoverServiceImpl implements CoverService {
             // 1) multipart가 null이면 장르별 default image
             Resource resource = resourceService.saveFile(multipartFile);
 
-            Cover newCover = coverRepository.save(
-                    new Cover(resource, coverId, cover.getLastPublishDate(),
-                            cover.getFirstPublishDate(), cover.getLikes(), cover.getViewCount(),
-                            coverModifyDto, cover.getMember(),
-                            genreRepository.getReferenceById(coverModifyDto.getGenreId())));
+            Resource resource = cover.getResource();
+            if (!(multipartFile == null || multipartFile.isEmpty())) {
+                resource = resourceService.saveFile(multipartFile);
+            } else if (coverModifyDto.getIsDefaultImage()) {
+                resource = null;
+            }
 
-            return findPreviousResourceInS3(newCover.getResource(), cover.getResource());
+            coverRepository.save(
+                new Cover(resource, coverId, cover.getLastPublishDate(),
+                    cover.getFirstPublishDate(), cover.getLikes(), cover.getViewCount(),
+                    coverModifyDto, cover.getMember(),
+                    genreRepository.getReferenceById(coverModifyDto.getGenreId())));
+
+            if (resource == null || !resource.getId().equals(cover.getResource().getId())) {
+                if (cover.getResource() != null) {
+                    resourceRepository.delete(cover.getResource());
+                }
+            }
+
+            return cover.getResource();
         }
 
-    }
-
-    private Resource findPreviousResourceInS3(Resource current, Resource previous) {
-
-        if (current == null || previous == null) {
-            return null;
-        }
-
-        if (!current.getUrl().equals(previous.getUrl())) {
-            return previous;
-        }
-
-        return null;
     }
 
     @Override
@@ -145,8 +162,18 @@ public class CoverServiceImpl implements CoverService {
 
     @Override
     public Page<CoverPurchasedDto> getPurchasedCover(Member member, Pageable pageable) {
-
-        return coverRepository.findPurchasedCoverByMember(member.getId(), pageable);
+        Page<CoverPurchasedDto> page = coverRepository.findPurchasedCoverByMember(member.getId(),
+            pageable);
+        if (!page.isEmpty()) {
+            page.getContent().stream().map(coverPurchasedDto ->  {
+                coverPurchasedDto.setThumbnail(
+                    coverPurchasedDto.getThumbnail() == null ?
+                        defaultImage.getImageByGenreName(coverPurchasedDto.getGenre())
+                        : coverPurchasedDto.getThumbnail());
+                    return coverPurchasedDto;
+            }).collect(Collectors.toList());
+        }
+        return page;
     }
 
     @Override
