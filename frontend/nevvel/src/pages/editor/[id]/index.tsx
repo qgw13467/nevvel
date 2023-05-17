cfbb729fc92225917641febfc96e49c1c274fde4
moo_miz <moo_miz@naver.com>
2023-05-17 09:36:10 +0900

    feat(fe):add initial my asset list axios

--- frontend/nevvel/src/pages/editor/[id]/index.tsx
+++ frontend/nevvel/src/pages/editor/[id]/index.tsx
@@ -4,9 +4,12 @@ import EditorMain from "@/src/components/editor/EditorMain";
 import styled from "styled-components";
 import { episode } from "editor";
 import { useRouter } from "next/router";
-
-
 import { mobile } from "@/src/util/Mixin";
+import springApi from "@/src/api";
+import { ImageAssetAtom, AudioAssetAtom } from "@/src/store/EditorAssetStore";
+import DummyAssetData_audio from "@/src/components/assetstore/DummyAssetData_Audio.json";
+import DummyAssetData_image from "@/src/components/assetstore/DummyAssetData_Image.json";
+import { useAtom } from "jotai";
 
 function index() {
   const router = useRouter();
@@ -22,6 +25,47 @@ function index() {
   useEffect(() => {
     scrollRef.current.scrollTop = scrollRef.current?.scrollHeight;
   }, [episode]);
+
+  const [assetImageData, setAssetImageData] = useAtom(ImageAssetAtom);
+  const [assetAudioData, setAssetAudioData] = useAtom(AudioAssetAtom);
+
+  // 이미지 get 요청
+  const getAssetImgData = async () => {
+    try {
+      const res = await springApi.get(
+        "assets/purchased-on?assettype=IMAGE&page=1&size=10&sort=createdDateTime"
+      );
+      if (res) {
+        console.log(res);
+        setAssetImageData(res.data.content);
+      }
+    } catch (error) {
+      console.log(error);
+      setAssetImageData(DummyAssetData_image.content);
+    }
+  };
+  // 오디오 get 요청
+  const getAssetAudioData = async () => {
+    try {
+      const res = await springApi.get(
+        "assets/purchased-on?assettype=AUDIO&page=1&size=10&sort=createdDateTime"
+      );
+      if (res) {
+        console.log(res);
+        setAssetAudioData(res.data.content);
+      }
+    } catch (error) {
+      console.log(error);
+      setAssetAudioData(DummyAssetData_audio.content);
+    }
+  };
+
+  useEffect(() => {
+    getAssetImgData();
+    getAssetAudioData();
+    // setAssetData(DummyAssetData_image.content)
+  }, []);
+
 
 
 
