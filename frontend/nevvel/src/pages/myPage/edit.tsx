19661a9ccb5aef81359783a25e7b3c5505181aa5
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-05-17 10:33:57 +0900

    feat: 마이페이지 라우터 # 추가

--- frontend/nevvel/src/pages/myPage/edit.tsx
+++ frontend/nevvel/src/pages/myPage/edit.tsx
@@ -10,12 +10,12 @@ function EditMyProfile() {
   const loginStatus = useAtomValue(loginAtom);
 
   // 로그아웃 상태인 경우 메인페이지로 리다이렉트
-  // const router = useRouter();
-  // useEffect(() => {
-  //   if (!loginStatus) {
-  //     router.push({ pathname: "/" });
-  //   }
-  // }, []);
+  const router = useRouter();
+  useEffect(() => {
+    if (!loginStatus) {
+      router.push({ pathname: "/#" });
+    }
+  }, []);
 
   return (
     <Wrapper>
